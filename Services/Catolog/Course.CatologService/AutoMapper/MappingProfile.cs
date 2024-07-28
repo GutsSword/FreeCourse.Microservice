a7ecaca2b0788ec04ee68f7b50f370dda672fbb3
Emirhan Akca <emirhanakkca@gmail.com>
2024-07-28 15:26:45 +0300

    Add project files.

---
+++ Services/Catolog/Course.CatologService/AutoMapper/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using FreeCourse.CatologService.Dtos;
+using FreeCourse.CatologService.Dtos.CategoryDtos;
+using FreeCourse.CatologService.Dtos.CourseDtos;
+using FreeCourse.CatologService.Entities;
+
+namespace FreeCourse.CatologService.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Category, CategoryDto>();
+
+            CreateMap<Category, CreateCourseDto>();
+            CreateMap<Category, CourseDto>();
+            CreateMap<Category, UpdateCourseDto>();
+
+            CreateMap<Category, FeatureDto>();
+            
+        }
+    }
+}
